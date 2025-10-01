cb7112b19dddec4a8889da7eba23a2b48062535f
김경영 <97999152+rudduddl@users.noreply.github.com>
2025-10-01 10:08:09 +0900

    [Gold V] Title: 토마토, Time: 2424 ms, Memory: 100524 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,15 +1,15 @@
 from collections import deque
-n, m = map(int, input().split())
+m, n = map(int, input().split())
 
-graph = [list(map(int, input().split())) for _ in range(m)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 
 
 queue = deque()
-for y in range(m):
-    for x in range(n):
+for y in range(n):
+    for x in range(m):
         if graph[y][x] == 1:
             queue.append((x,y))
 
@@ -17,7 +17,7 @@ while queue:
     x, y = queue.popleft()
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and graph[ny][nx] == 0:
+        if 0 <= nx < m and 0 <= ny < n and graph[ny][nx] == 0:
             graph[ny][nx] = graph[y][x] + 1
             queue.append((nx, ny))
 
