d50928f26214c173754d58453ee225b19682b7d0
김경영 <97999152+rudduddl@users.noreply.github.com>
2026-01-01 14:39:42 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 196 ms, Memory: 18020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.hasNextInt()) {
+
+            int n = sc.nextInt();
+
+            for(int i=1;i<=n;i++){
+                String star = "*";
+                for(int j=1;j<i;j++){
+                    star += "*";
+                }
+                System.out.println(star);
+            }
+
+        }
+
+    }
+}
