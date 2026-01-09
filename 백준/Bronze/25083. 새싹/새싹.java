4d1d59042c69ba394e34302b04f14a81f8e9b6a0
김경영 <97999152+rudduddl@users.noreply.github.com>
2026-01-09 10:46:18 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("         ,r'" + '"'+"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+    }
+
+}
