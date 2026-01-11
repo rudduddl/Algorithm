fd49618662b70743487910e541239a6d1aa69b19
김경영 <97999152+rudduddl@users.noreply.github.com>
2026-01-11 15:26:19 +0900

    [Bronze I] Title: 세로읽기, Time: 132 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] words = new String[5];
+
+        for(int i=0;i<5;i++){
+            words[i] = br.readLine();
+        }
+        
+        for(int j=0;j<15;j++){
+            for(int i=0;i<5;i++){
+                if(j < words[i].length()){
+                    System.out.print(words[i].charAt(j));
+                }
+            }
+        }
+        
+    }
+
+}
